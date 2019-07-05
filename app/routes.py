27145f8eac401f0a6503917cddb079eeeff959cb
martahan <48981864+martahan@users.noreply.github.com>
2019-07-05 12:54:05 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -228,3 +228,12 @@ def list_function_calls_from_verdict_and_path(verdict, path):
 	template_with_data = render_template("function_list.html", data=map_structure, truth_map={1:"Satisfaction", 0:"Violation"})
 
 	return template_with_data
+
+
+
+@app_object.route("/list_functions2")
+def list_functions_2():
+	return jsonify(database.list_functions2())
+
+
+
