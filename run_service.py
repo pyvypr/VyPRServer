04ede8b40bae06dcf0f98d248959eaec355159e7
Marta Han <mahan@cmglen01.cern.ch>
2020-03-24 18:16:43 +0100

    added the option to parse the name of the database as an argument

--- run_service.py
+++ run_service.py
@@ -1,8 +1,16 @@
 """
 Main module for a test Flask-based web application.
 """
-
+import app
 from app import app_object
+import argparse
+
+parser = argparse.ArgumentParser(prog="Running the server side of VyPR")
+parser.add_argument("--db", type=str, help="name of the database containing verdicts", required=False)
+args = parser.parse_args()
+
+if args.db:
+    app.database_string = args.db
 
 if __name__ == "__main__":
 
