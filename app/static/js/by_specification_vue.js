85a6832c0f0842a8036b2b4d642883820d922dbe
Marta Han <mahan@cmglen01.cern.ch>
2020-05-07 14:41:17 +0200

    updated components which list functions - uses jquery - might have to change this

--- app/static/js/by_specification_vue.js
+++ app/static/js/by_specification_vue.js
@@ -15,13 +15,44 @@ Vue.component("machine-function-property", {
         <div class="list-group" id="function-list">
           <div id="function-list-data"></div>
           <div class="tab">
-            <button v-for="(value,key) in tree" class="tablinks">{{key}}</button>
+            <button v-for="(value,key) in tree" class="tablinks" @click="selectTab(key)">{{key}}</button>
           </div>
           <subtree v-for="(value,key) in tree" :key="key" :id="key" :content="value"> </subtree>
         </div>
       </div>
-    </div>`
-  }
+    </div>`,
+	methods : {
+		selectTab: function(selectedTab){
+			// selectedTab contains the ID of the tabcontent element which needs to be displayed
+			console.log(selectedTab)
+			var i, tabcontent, tablinks;
+			tabcontent = $(".tabcontent");
+			for (i = 0; i < tabcontent.length; i++) {
+				tabcontent[i].setAttribute('style', 'display: none');
+			}
+			tablinks = $(".tablinks")
+			for (i=0; i<tablinks.length; i++) {
+				tablinks[i].className = "tablinks";
+			}
+			if (selectedTab != "") {
+				$("#"+selectedTab).attr('style', 'display = "block"');
+				tablinks = $(".tablinks");
+				for (i=0; i<tablinks.length; i++){
+					if (tablinks[i].innerHTML == selectedTab){
+						tablinks[i].className += " active"
+					}
+				}
+			}
+		}
+	},
+	mounted() {
+		var machine_keys=[];
+		for (key in this.tree){
+			machine_keys.push(key)
+		}
+		this.selectTab(machine_keys[0]);
+	}
+}
 )
 
 Vue.component("subtree",{
@@ -33,30 +64,61 @@ Vue.component("subtree",{
 })
 
 Vue.component("subtreelevel", {
-  props: ['htmlcontent', 'path', 'panelid', 'stylepadding', 'keyname', 'islast'],
+	/*recursive component, properties store the following information:
+	htmlcontent - function tree based on which the html of the element is built, updated for each level
+	path - the path through the function tree to the current element
+	panelid, stylepadding - strings which are used to define the header on each level
+	keyname - title in the header, taken as a dictionary key on the current tree level
+
+	after adding the header for the current level, iterate through the lower level to repeat the process
+	if the subtreelist is empty, this is an indicator that we reached the last level
+	- this means it is time to display the specifications monitored for the function - the recursion stops here*/
+  props: ['htmlcontent', 'path', 'panelid', 'stylepadding', 'keyname'],
+	template: `
+		<div class="panel panel-default" style="inherit">
+			<div v-if="keyname" class="panel-heading">
+				<h3 class="panel-title" :id="this.panelid" :style="this.stylepadding"> {{this.keyname}} </h3>
+			</div>
+			<div class="panel-body">
+				<div class="list-group" :id="path">
+					<subtreelevel v-for="(level,index) in this.subtreeslist" :key="index" :htmlcontent="level.nextcontent"
+					:path="level.nextpath" :panelid="level.panelid" :stylepadding="level.stylepadding"
+					:keyname="level.keyname"></subtreelevel>
+					<div v-if="!this.subtreelist" v-html="this.subHTML">
+					</div>
+				</div>
+			</div>
+		</div>
+		`,
   data() {
+		// take subtree and path from properties parsed to this component
     var subtree = JSON.parse(JSON.stringify(this.htmlcontent));
     var path = this.path;
-    var padding = String(path.length * 2) + "px";
+    var padding = String(path.split("-").length * 20) + "px";
+
+		// check if the last level is reached - if it is, return an empty subtrees list to indicate this
+		// also return the HTML needed to build the element with the specification
     if(Array.isArray(subtree)) {
   		var html_string = ""
   		for(var i=0; i<subtree.length; i++) {
   			var str = subtree[i][2];
   			str = decodeHTML(str);
-
   			html_string += ('<button type="button" class="list-group-item" function-id="' + subtree[i][0] +
   				'" style="padding-left:' + padding + '">' + str + '</button>');
   		}
   		this.subHTML = html_string;
-      return {subtreeslist : []}
+      return {subHTML: html_string, subtreeslist:[]}
+
+		// if this is not the last level, iterate through the keys in the subtree and return the list
+		// based on which new subtree components will be defined
   	} else {
-      var dicts_list = [];
   		var keys = [];
   		for (var key in subtree) {
   			keys.push(key);
   		}
   		console.log("path="+path+"; keys="+ keys);
 
+			var dicts_list = [];
   		for(var i=0; i<keys.length; i++) {
   			var key = keys[i];
   			// create list inside the current list in the dom
@@ -64,24 +126,13 @@ Vue.component("subtreelevel", {
 				var panel_id = "external-" + new_path;
 				var style_padding = "padding-left: " + padding;
         console.log(JSON.stringify(subtree[key]))
-        dicts_list.push({keyname: key, nextcontent : subtree[key], nextpath : new_path, stylepadding : style_padding, panelid : panel_id});
+        dicts_list.push({keyname: key,
+					nextcontent : subtree[key], nextpath : new_path,
+					stylepadding : style_padding, panelid : panel_id});
   		}
       return {subtreeslist : dicts_list}
   	}
-  },
-	template: `
-		<div v-if="islast" v-html="subHTML">
-		</div>
-		<div v-else class="panel panel-default" style="inherit">
-			<div class="panel-heading">
-				<h3 class="panel-title" :id="this.panelid" :style="this.stylepadding"> {{this.keyname}} </h3>
-			</div>
-			<div class="panel-body">
-				<div class="list-group" :id="path">
-					<subtreelevel v-for="(level,index) in subtreeslist" :key="index" :htmlcontent="level.nextcontent" :path="level.nextpath" :panelid="level.panelid" :stylepadding="level.stylepadding" :keyname="level.keyname" :islast="false"></subtreelevel>
-				</div>
-			</div>
-		</div>`
+  }
 })
 
 Vue.component("function-calls", {
