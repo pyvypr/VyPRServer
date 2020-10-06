1f1a7ebccabecd15e8a7c893f23a808af9ccc9c2
Marta Han <han.marta@gmail.com>
2020-10-06 10:11:14 +0200

    commenting out a print statement

--- app/database/web.py
+++ app/database/web.py
@@ -266,7 +266,7 @@ def web_list_functions(tests = None):
 
         else:
             atom_str = prop.HTMLrepr()
-            print(prop.operands[0].HTMLrepr())
+            #print(prop.operands[0].HTMLrepr())
             vars = ''
             foralls = []
 
